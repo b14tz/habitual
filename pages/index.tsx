e994ec36730db985f5b3272376a65e441adef18b
Marshall Blatz <marshallpblatz@gmail.com>
2023-05-23 14:01:49 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+type Props = {}
+
+export default function Index({}: Props) {
+  return (
+    <>
+        <div className="flex w-screen h-screen justify-center items-center">
+            <h1>To Do List</h1>
+        </div>
+    </>
+  )
+}
