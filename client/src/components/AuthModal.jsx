7f863e3091239f4f2ad52c31b802142f57f9ddf1
Marshall Blatz <marshallpblatz@gmail.com>
2023-06-06 12:47:59 -0500

    progress squares render properly

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -49,8 +49,8 @@ export default function AuthModal(props) {
 
     return props.trigger ? (
         <>
-        <div className="fixed top-0 left-0 flex w-full h-full justify-center items-center bg-black/[.30]">
-            <div className='min-w-[500px] min-h-[250px] rounded-md bg-white-2 dark:bg-black-2 z-50 flex flex-col justify-center items-center p-4'>
+        <div className="z-50 fixed top-0 left-0 flex w-full h-full justify-center items-center bg-black/[.30]">
+            <div className='min-w-[500px] min-h-[250px] rounded-md bg-white-2 dark:bg-black-2 flex flex-col justify-center items-center p-4'>
                 <div className="w-[90%] flex flex-row justify-between items-center">
                     <h2 className='text-3xl'>{ hasAccount ? "Log in" : "Sign up" }</h2>
                     <button className="drop-shadow-md rounded-full"
--- client/src/components/HabitGridCell.jsx
+++ client/src/components/HabitGridCell.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default function HabitGridCell(props) {
-    const classes = `w-5 h-5 bg-black-1 dark:bg-white-1 m-1 opacity-[${props.progress}]`
+    const divStyle = {
+        opacity: props.progress
+    }
     return (
         <>
-            <div className={classes}></div>
+            <div className={`w-5 h-5 bg-black-1 dark:bg-white-1 m-1`} style={divStyle}></div>
         </>
     )
 }
