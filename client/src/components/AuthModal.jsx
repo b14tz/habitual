e3b2c381a92e019824d1fb72e86994d98b1cbe47
Marshall Blatz <marshallpblatz@gmail.com>
2023-06-08 11:52:19 -0500

    fixed import issue again

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useAuth } from '../contexts/authContext';
+import { useAuth } from '../contexts/AuthContext';
 import { createUser } from '../interfaces/userInterface';
 
 
